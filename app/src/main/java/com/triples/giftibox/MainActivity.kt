e99c11516df4f598d6b751ca31865db370b00699
SHINMH <sky95012@gmail.com>
2021-06-03 12:16:28 +0900

    Add navi_graph

--- app/src/main/java/com/triples/giftibox/MainActivity.kt
+++ app/src/main/java/com/triples/giftibox/MainActivity.kt
@@ -2,6 +2,7 @@ package com.triples.giftibox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
