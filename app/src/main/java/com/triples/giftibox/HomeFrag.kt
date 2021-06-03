47f072162d33db277a80c8cc726b160631e2ad3e
SHINMH <sky95012@gmail.com>
2021-06-03 12:38:14 +0900

    Add change home to map

--- app/src/main/java/com/triples/giftibox/HomeFrag.kt
+++ app/src/main/java/com/triples/giftibox/HomeFrag.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,12 @@ class HomeFrag : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.frag_home, container, false)
+        val view = inflater.inflate(R.layout.frag_home, container, false)
+        //  clickListener 설정
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_homeFrag_to_mapFrag)
+        }
+        return view
     }
 
     companion object {
